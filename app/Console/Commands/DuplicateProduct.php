4715cfb2661d0a9390fca640e72f2f7d2b8eaf9f
Fariz Dev <farishidayat1@gmail.com>
2024-04-24 09:39:04 +0700

    update

---
+++ app/Console/Commands/DuplicateProduct.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class DuplicateProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:duplicate-product';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $product = Product::first();
+
+        for($i = 0; $i <= 20; $i++) {
+            Product::create([
+                'name' => $product->name,
+                'category' => $product->category,
+                'purchase_price' => $product->purchase_price,
+                'selling_price' => $product->selling_price,
+                'qty' => $product->qty,
+                'image_url' => $product->image_url,
+            ]);
+            $this->info($i);
+        }
+    }
+}
